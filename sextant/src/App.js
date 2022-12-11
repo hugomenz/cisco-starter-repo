9fac716297391f3036969c2cdfc6188d44fe17dc
Hugo Martin <hugomartin.menz@gmail.com>
2022-12-11 08:07:03 +0100

    Updated App

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { textFields } from "./utils";
 
 function App() {
+  const REACT_APP_DOCUMENT_TITLE = "Sextant";
+
+  React.useEffect(() => {
+    document.title = REACT_APP_DOCUMENT_TITLE;
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">{REACT_APP_DOCUMENT_TITLE}</header>
+      <body>
+        <div>
+          <h1>Cisco Intro to Software Engineering</h1>
+        </div>
+        <div>
+          {textFields.map((element) => {
+            return (
+              <div className="block">
+                <h4>{element.title}</h4>
+                <div>
+                  <span>{element.content}</span>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </body>
     </div>
   );
 }
---
+++ sextant/src/utils.js
@@ -0,0 +1,26 @@
+export const textFields = [
+  {
+    title: "Project Initialization",
+    content: "Set up your local development environment to prepare for work.",
+  },
+  {
+    title: "Design the Sextant Frontend",
+    content: "Stitch together a series of reusable React components to create a frontend for Sextant.",
+  },
+  {
+    title: "Display the User's Public IP",
+    content: "Set up your local development environment to prepare for work.",
+  },
+  {
+    title: "Project Initialization",
+    content: "Retrieve and display a public IP in Sextant.",
+  },
+  {
+    title: "Stream Data from a Networked Service",
+    content: "Use WebSocket to connect to an external service.",
+  },
+  {
+    title: "Present Your Work",
+    content: "Record a short video showcasing what you've built.",
+  },
+];
